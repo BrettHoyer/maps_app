9cd84b3913e99d167e57ce68cc4ff085c218fb20
Brett Hoyer <jbh5079@gmail.com>
2012-10-24 19:36:22 -0500

    updated routes and added pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Newapp::Application.routes.draw do
 
+  root :to => "pages#home"
+
+  match '/home' => "pages#home"
+
   get '/information', :controller => 'people', :action => 'all'
   post '/information', :controller => 'people', :action => 'create'
   get '/information/new', :controller => 'people', :action => 'add_info'
